dcbb1c379def75a1abfbf314867192e4e1853c40
Adilet312 <adiletm@hotmail.com>
2020-09-12 01:04:13 -0700

    Added addEventListener when user clicks icon changes.

--- src/Questions.js
+++ src/Questions.js
@@ -1,8 +1,23 @@
 
 import './styles.css';
+let main = document.querySelector('main');
+document.querySelector('.btn').addEventListener('click',function(event){
+  let answer = main.lastElementChild;
+  let rowUp = event.currentTarget.firstElementChild;
+  let rowDown = event.currentTarget.lastElementChild;
+  console.log(event.currentTarget)
+  if(answer.classList.contains('answer-show') && rowUp.classList.contains('rowUp-hidden') && rowDown.classList.contains('rowDown')){
+    answer.classList.remove('answer-show');
+    rowUp.classList.remove('rowUp-hidden');
+    rowDown.classList.remove('rowDown');
+  }
+  else{
+    answer.classList.add('answer-show');
+    rowUp.classList.add('rowUp-hidden');
+    rowDown.classList.add('rowDown');
 
-document.querySelector('.rowUp').addEventListener('click',function(event){
-  console.log(event.target)
+  }
+
 },false);
 
 /*  event.target.style.display = "none";
