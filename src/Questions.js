15607f58b6abcd62897d790966aafb2bc19d27a6
Adilet312 <adiletm@hotmail.com>
2020-09-11 23:42:10 -0700

    Added Initial phase.

---
+++ src/Questions.js
@@ -0,0 +1,9 @@
+
+import './styles.css';
+
+document.querySelector('.rowUp').addEventListener('click',function(event){
+  console.log(event.target)
+},false);
+
+/*  event.target.style.display = "none";
+  document.querySelector('.rowDown').style.display = "block";*/
